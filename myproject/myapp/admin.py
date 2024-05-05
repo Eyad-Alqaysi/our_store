18f0112f44c17a505d273f65a68fdb8e4446c0e0
WALEED-SAMA <wleedzx44@gmail.com>
2024-05-05 16:37:59 +0300

    Admin updates

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Developer, Genre, Game, GameInstance
 
-# Register your models here.
+class GameInstanceInline(admin.TabularInline):
+    model = GameInstance
+    extra = 0
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'developer', 'display_genre', 'release_date')
+
+    def display_genre(self, obj):
+        """Creates a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join([genre.name for genre in obj.genre.all()[:3]])  # Adjust the slice for performance if needed
+    display_genre.short_description = 'Genre'
+
+    inlines = [GameInstanceInline]
+
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = ('name', 'founded_date', 'headquarters')
+
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+class GameInstanceAdmin(admin.ModelAdmin):
+    list_display = ('game', 'edition', 'status')
+    list_filter = ('status',)
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Developer, DeveloperAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(GameInstance, GameInstanceAdmin)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,35 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Developer(models.Model):
+    name = models.CharField(max_length=100)
+    founded_date = models.DateField(null=True, blank=True)
+    headquarters = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    developer = models.ForeignKey(Developer, on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the game")
+    release_date = models.DateField(null=True, blank=True)
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this game")
+
+    def __str__(self):
+        return self.title
+
+class GameInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular game across whole library")
+    game = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True)
+    edition = models.CharField(max_length=200, null=True, blank=True)
+    status = models.CharField(max_length=20, choices=[('available', 'Available'),('sold out', 'Sold out'),('preorder', 'Pre-order')], blank=True, default='available')
+
+    def __str__(self):
+        return f'{self.game.title} - {self.edition} Edition'

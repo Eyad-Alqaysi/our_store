ab93ff3921629baab0b59f149cdff06f886de6d5
Moriarty12k <107506703+Moriarty12k@users.noreply.github.com>
2024-05-05 18:05:54 +0300

    need to modify the html & css only

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
+    path('games/', views.GameListView.as_view(), name='games'),
+    path('developers/', views.DeveloperListView.as_view(), name='developers'),
+    path('game/<int:pk>', views.GameDetailView.as_view(), name='game_detail'),
+    path('developer/<int:pk>', views.DeveloperDetailView.as_view(), name='developer_detail'),
 ]
+
+
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,26 @@
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Hello, world. You're at the home page.")
+from django.shortcuts import render
+from django.views import generic
+from .models import Game, Developer
+
+def index(request):
+    # Simple context for now, you can add more details later
+    return render(request, 'index.html')
+
+class GameListView(generic.ListView):
+    model = Game
+    template_name = 'games/game_list.html'
+
+class DeveloperListView(generic.ListView):
+    model = Developer
+    template_name = 'developers/developer_list.html'
+
+class GameDetailView(generic.DetailView):
+    model = Game
+    template_name = 'myapp/game_detail.html'  # Correct path based on your app's name
+
+
+class DeveloperDetailView(generic.DetailView):
+    model = Developer
+    template_name = 'developers/developer_detail.html'
